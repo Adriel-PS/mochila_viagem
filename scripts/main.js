fa615ec36586b2285e5edfec94c4ea4d9d678684
adriel <adriel@taqe.com.br>
2023-09-05 00:53:53 -0300

    atualização do curso 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 let form = document.getElementById('novoItem')
 const lista = document.getElementById("lista");
 console.log(form);
+const itens = [];
 
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault()
@@ -9,17 +10,21 @@ form.addEventListener("submit", (evento)=>{
     nome = evento.target.elements['nome'].value;
     quanditade =evento.target['quantidade'].value;
     criaElemento(nome,quanditade)
+
+    evento.target.elements['nome'].value = "";
+    evento.target.elements['quantidade'].value = "";
+    
 } )
 
-function criaElemento(nome,quanditade){
+function criaElemento(nome,quantidade){
     console.log(nome);
-    console.log(quanditade);
+    console.log(quantidade);
 
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
 
     const numero = document.createElement('strong');
-    numero.innerHTML= quanditade;
+    numero.innerHTML= quantidade;
 
     novoItem.appendChild(numero)
     novoItem.innerHTML += nome;
@@ -27,4 +32,13 @@ function criaElemento(nome,quanditade){
     const lista = document.getElementById("lista");
 
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade" : quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
